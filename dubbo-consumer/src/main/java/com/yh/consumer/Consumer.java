d0de74a6a488e0b515ceb161de7a9c5c9fc44238
yanghui939 <1366676359@qq.com>
2020-08-02 22:20:11 +0800

    maven方式的dubbo

--- dubbo-consumer/src/main/java/com/yh/consumer/Consumer.java
+++ dubbo-consumer/src/main/java/com/yh/consumer/Consumer.java
@@ -5,6 +5,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
 
+/**
+ * 消费者
+ */
 public class Consumer {
     public static void main(String[] args) throws IOException {
         ClassPathXmlApplicationContext context= new ClassPathXmlApplicationContext(
